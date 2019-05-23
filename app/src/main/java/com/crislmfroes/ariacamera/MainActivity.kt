083a187f81440fb1384346563d1cd97fa2ad377e
crislmfroes <cris.lima.froes@gmail.com>
2019-05-23 10:16:47 -0300

    modificou .gitignore e adicionou detecção facial

--- app/src/main/java/com/crislmfroes/ariacamera/MainActivity.kt
+++ app/src/main/java/com/crislmfroes/ariacamera/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.crislmfroes.ariacamera
 
+import android.content.Context
+import android.hardware.camera2.CameraManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.util.SparseIntArray
+import android.view.Surface
+import android.view.View
 import android.widget.TextView
 import com.google.firebase.ml.vision.FirebaseVision
 import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata
+import com.google.firebase.ml.vision.face.FirebaseVisionFace
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions
 import io.fotoapparat.Fotoapparat
 import io.fotoapparat.configuration.CameraConfiguration
 import io.fotoapparat.preview.Frame
@@ -59,10 +67,50 @@ class MainActivity : AppCompatActivity() {
             }
     }
 
-    fun onClickText() {
+    private fun detectFaces(frame : Frame) {
+        val metadata = FirebaseVisionImageMetadata.Builder()
+            .setFormat(FirebaseVisionImageMetadata.IMAGE_FORMAT_NV21)
+            .setRotation(FirebaseVisionImageMetadata.ROTATION_0)
+            .setWidth(frame.size.width)
+            .setHeight(frame.size.height)
+            .build()
+        val fireImage = FirebaseVisionImage.fromByteArray(frame.image, metadata)
+        val detectorOptions = FirebaseVisionFaceDetectorOptions.Builder()
+            .setClassificationMode(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
+            .build()
+        val detector = FirebaseVision.getInstance().getVisionFaceDetector(detectorOptions)
+        val result = detector.detectInImage(fireImage)
+            .addOnSuccessListener {
+                val faces = it
+                val faceCount = faces.size
+                var smileCount = 0
+                for (face in faces) {
+                    if (face.smilingProbability != FirebaseVisionFace.UNCOMPUTED_PROBABILITY) {
+                        if (face.smilingProbability >= 0.5) {
+                            smileCount += 1
+                        }
+                    }
+                }
+                textView!!.text = "%i rostos na camera, %i estão sorrindo.".format(faceCount, smileCount)
+            }
+            .addOnFailureListener {
+                Log.e("Error", "Error on detectFaces", it)
+            }
+    }
+
+    fun onClickText(v : View) {
         val config = CameraConfiguration(
             frameProcessor = {
                 readText(it)
+            }
+        )
+        apparat!!.updateConfiguration(config)
+    }
+
+    fun onClickFaces(v : View) {
+        val config = CameraConfiguration(
+            frameProcessor = {
+                detectFaces(it)
             }
         )
         apparat!!.updateConfiguration(config)
